4a15dc0657034c8bdcd832e90c0bfdbc5e423276
Rishabh Rao <rishabhsrao@yahoo.com>
2011-08-04 22:29:01 +0530

    Added more logging.

--- src/bee/ris/rushhour/data/Place.java
+++ src/bee/ris/rushhour/data/Place.java
@@ -2,6 +2,7 @@ package bee.ris.rushhour.data;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -44,6 +45,12 @@ class Place {
 	 */
 	protected static final int MAX_PLACE_NAME_LENGTH = 32;
 	/**
+	 * The maximum number of characters that a place name can contain.
+	 * @author Rishabh Rao
+	 * @since 0.0.5
+	 */
+	protected static final int MAX_PLACE_ID_LENGTH = 32;
+	/**
 	 * Creates and initializes the <code>Place</code> object for counting.
 	 * @param placeId A unique identification code for a place.
 	 * @param placeName The venue name of this <code>Place</code>.
@@ -51,10 +58,19 @@ class Place {
 	 * @since 0.0.5
 	 */
 	public Place(final String placeId, final String placeName) {
-		l.entering(Place.class.getSimpleName(), "Place", placeId, placeName)
+		if(l.isLoggable(Level.FINER)) {				 // NOI18N
+			l.entering(Place.class.getSimpleName(), "Place",
+					new Object[] {placeId, placeName});
+		}
+		
+		this.setPlaceId(placeId);
 		this.setPlaceName(placeName);
 		this.crowdCount = new Timely<Integer>();
 		this.refreshCount = new Timely<Integer>();
+		
+		if(l.isLoggable(Level.FINER)) {				// NOI18N
+			l.exiting(Place.class.getSimpleName(), "Place");
+		}
 	}
 	/**
 	 * Sets the <code>placeName</code>.
@@ -64,11 +80,15 @@ class Place {
 	 */
 	private void setPlaceName(final String placeName)
 			throws IllegalArgumentException {
+		if(l.isLoggable(Level.FINER)) {				 // NOI18N
+			l.entering(Place.class.getSimpleName(), "setPlaceName", placeName);
+		}
+		
 		// Don't allow nulls or blanks.
 		if(null == placeName || 0 == placeName.length()) {
 			throw new IllegalArgumentException(ResourceBundle.
 				getBundle("i18n").  // NOI18N
-				getString("Place.setPlaceName.IllegalArgumentException"));
+				getString("Place.set.IllegalArgumentException"));
 		}
 
 		// Don't allow long names.
@@ -76,8 +96,44 @@ class Place {
 			this.placeName = placeName.substring(0, Place.MAX_PLACE_NAME_LENGTH);
 		} else {
 			this.placeName = placeName;
-		}		
+		}
+		
+		if(l.isLoggable(Level.FINER)) {				// NOI18N
+			l.exiting(Place.class.getSimpleName(), "setPlaceName", this.placeName);
+		}
 	}
-	
+	/**
+	 * Sets the <code>placeName</code>.
+	 * @param placeName The venue that this <code>Place</code> holds data for.
+	 * @author Rishabh Rao
+	 * @since 0.0.5
+	 */
+	private void setPlaceId(final String placeId)
+			throws IllegalArgumentException {
+		if(l.isLoggable(Level.FINER)) {				 // NOI18N
+			l.entering(Place.class.getSimpleName(), "setPlaceId", placeId);
+		}
+		
+		// Don't allow nulls or blanks.
+		if(null == placeId || 0 == placeId.length()) {
+			throw new IllegalArgumentException(ResourceBundle.
+				getBundle("i18n"). // NOI18N
+				getString("Place.set.IllegalArgumentException")); // NOI18N
+		}
+
+		// Don't allow long names.
+		if(Place.MAX_PLACE_ID_LENGTH <= placeId.length()) {
+			this.placeId = placeId.substring(0, Place.MAX_PLACE_NAME_LENGTH);
+		} else {
+			this.placeId = placeId;
+		}
+		
+		if(l.isLoggable(Level.FINER)) {				// NOI18N
+			l.exiting(Place.class.getSimpleName(), "setPlaceId", this.placeId);
+		}
+	}
+	/**
+	 * The logger for the <code>Place</code> class.
+	 */
 	private static Logger l = Logger.getLogger(Place.class.getSimpleName());
 }
